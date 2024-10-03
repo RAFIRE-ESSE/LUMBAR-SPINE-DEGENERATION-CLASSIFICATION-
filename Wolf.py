c6499ea7ebcaf14674eabb8b5d125547f4fb995e
RAFIRE <168618384+RAFIRE-ESSE@users.noreply.github.com>
2024-10-03 09:19:30 +0530

    Update Wolf.py

--- Wolf.py
+++ Wolf.py
@@ -12,14 +12,22 @@ if os.path.exists(f"train_wolf")==False:
 		os.mkdir(f"train_wolf/spinal_canal_stenosis/normal")
 		os.mkdir(f"train_wolf/spinal_canal_stenosis/Moderate")
 		os.mkdir(f"train_wolf/spinal_canal_stenosis/Severe")
-		os.mkdir(f"train_wolf/neural_foraminal_narrowing")
-		os.mkdir(f"train_wolf/neural_foraminal_narrowing/normal")
-		os.mkdir(f"train_wolf/neural_foraminal_narrowing/Moderate")
-		os.mkdir(f"train_wolf/neural_foraminal_narrowing/Severe")
-		os.mkdir(f"train_wolf/subarticular_stenosis")
-		os.mkdir(f"train_wolf/subarticular_stenosis/normal")
-		os.mkdir(f"train_wolf/subarticular_stenosis/Moderate")
-		os.mkdir(f"train_wolf/subarticular_stenosis/Severe")
+		os.mkdir(f"train_wolf/left_neural_foraminal_narrowing")
+		os.mkdir(f"train_wolf/left_neural_foraminal_narrowing/normal")
+		os.mkdir(f"train_wolf/left_neural_foraminal_narrowing/Moderate")
+		os.mkdir(f"train_wolf/left_neural_foraminal_narrowing/Severe")
+		os.mkdir(f"train_wolf/right_neural_foraminal_narrowing")
+		os.mkdir(f"train_wolf/right_neural_foraminal_narrowing/normal")
+		os.mkdir(f"train_wolf/right_neural_foraminal_narrowing/Moderate")
+		os.mkdir(f"train_wolf/right_neural_foraminal_narrowing/Severe")
+		os.mkdir(f"train_wolf/left_subarticular_stenosis")
+		os.mkdir(f"train_wolf/left_subarticular_stenosis/normal")
+		os.mkdir(f"train_wolf/left_subarticular_stenosis/Moderate")
+		os.mkdir(f"train_wolf/left_subarticular_stenosis/Severe")
+		os.mkdir(f"train_wolf/right_subarticular_stenosis")
+		os.mkdir(f"train_wolf/right_subarticular_stenosis/normal")
+		os.mkdir(f"train_wolf/right_subarticular_stenosis/Moderate")
+		os.mkdir(f"train_wolf/right_subarticular_stenosis/Severe")
 
 for i in zip(numpy.array(label["study_id"]),numpy.array(label["series_id"])
 	,numpy.array(label['instance_number']),numpy.array(label['condition']),numpy.array(label['level']),range(len(numpy.array(label['level'])))):
@@ -32,10 +40,16 @@ for i in zip(numpy.array(label["study_id"]),numpy.array(label["series_id"])
 	print(f"{i[3]}/{wolf}/{w_}.jpg")
 	if i[3]=="Spinal Canal Stenosis":
 		cv2.imwrite(f"train_wolf/spinal_canal_stenosis/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
-	if i[3]=="Right Neural Foraminal Narrowing" or i[3]=="Left Neural Foraminal Narrowing":
-		cv2.imwrite(f"train_wolf/neural_foraminal_narrowing/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
-	if i[3]=="Left Subarticular Stenosis" or i[3]=="Right Subarticular Stenosis":
-		cv2.imwrite(f"train_wolf/subarticular_stenosis/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
+	if i[3]=="Right Neural Foraminal Narrowing":
+		cv2.imwrite(f"train_wolf/right_neural_foraminal_narrowing/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
+	if i[3]=="Left Neural Foraminal Narrowing":
+		cv2.imwrite(f"train_wolf/left_neural_foraminal_narrowing/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
+	if i[3]=="Left Subarticular Stenosis":
+		cv2.imwrite(f"train_wolf/left_subarticular_stenosis/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
+	if i[3]=="Right Subarticular Stenosis":
+		cv2.imwrite(f"train_wolf/right_subarticular_stenosis/{wolf}/{i[5]}_{w_}.jpg",cv2.normalize(pydicom.dcmread(f"train_images/{i[0]}/{i[1]}/{i[2]}.dcm").pixel_array, None, 0, 255, cv2.NORM_MINMAX).astype('uint8'))
+
+
 
 
 
